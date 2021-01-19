505f35314c4d7ef52d467090383ebb376b6d566e
Huldson Richard <huldson-_-@hotmail.com>
2021-01-19 12:37:26 -0300

    sitema de notas de uma escola

---
+++ exercicios/exe09.js
@@ -0,0 +1,51 @@
+
+y=[40,54,63,40,73,34,35,61]
+
+function notasaluno(notas){
+
+for(i=0;i<notas.length;i++){
+if(notas[i]<38){
+console.log("reprovado com nota: "+ notas[i])
+
+} else if(notas[i]>=38 && notas[i]<=40){
+
+notas[i]=40
+console.log('aprovado com nota : '+notas[i])
+
+}else if(notas[i]>40){
+ 
+console.log("aprovado com nota : "+arrendondar(notas[i])) 
+
+
+}
+
+
+
+
+
+}
+
+function arrendondar(x){
+
+if (x%5 == 3){
+    x=x+2
+    return x
+}
+else if (x%5 == 4){
+x=x+1
+ return x
+}
+else{
+    return x
+}
+
+
+
+}
+
+
+}
+
+
+
+notasaluno(y)

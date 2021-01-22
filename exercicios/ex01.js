8b8d3c770570bb68e9328b61feebb9d47243f3e8
Huldson Richard <huldson-_-@hotmail.com>
2021-01-22 15:15:36 -0300

    corrigido o bug de tipagem

--- exercicios/ex01.js
+++ exercicios/ex01.js
@@ -1,6 +1,9 @@
 function calcularOperacoes (operador1, operador2) {
+    
+    if((true==(typeof operador1 === 'number')&&true==(typeof operador2 === 'number'))){
     console.log(operador1+operador2, operador1-operador2, operador1*operador2, operador1/operador2);
-}
+}}
 
-calcularOperacoes(1, 2)
+calcularOperacoes(2, 2)
+
 

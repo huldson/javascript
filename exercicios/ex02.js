1dd9fe709dd0da905545ba0339529334895d09dd
Huldson Richard <huldson-_-@hotmail.com>
2021-01-22 17:41:15 -0300

    troca do == para ===

--- exercicios/ex02.js
+++ exercicios/ex02.js
@@ -1,7 +1,7 @@
 function classificaTriangulo (lado1, lado2, lado3) {
-    if(lado1 == lado2 && lado2 == lado3){
+    if(lado1 === lado2 && lado2 === lado3){
         return 'Equilátero'
-    } else if (lado1 == lado2 || lado2 == lado3 || lado1 == lado3) {
+    } else if (lado1 === lado2 || lado2 === lado3 || lado1 === lado3) {
         return 'Isósceles'        
     }else { 
         return 'Escaleno'
